ddd37030482b925218072a2f9023c11c350ea83c
Eduardo Garcia <ntarticpnguin@proton.me>
2025-05-30 06:53:21 -0600

    Update aliases.sh
    typos, added comments and bat as requirements

--- aliases.sh
+++ aliases.sh
@@ -1,13 +1,13 @@
 # You should know these commands before committing to adopt their shortened version
 # If  you need to run the original commands just do so by escaping the command, ie
-# df here will fun a df -hT but to run original df, do:
+# df here will run a df -hT but to run original df, do:
 # \df and so on...
 #
-# Requirements: htop
+# Requirements: htop, bat
 # sudo dnf5 install htop
 
 alias reloadb='source ~/.bashrc'
-# ls * --color=always may mess some i/o while piping
+# ls * --color=always may mess some i/o while piping, better to use --color-auto
 alias ll='ls -lrth --color=auto'  # list, reverse order, timestamp, humar readalbe
 alias la='ls -lrtha --color=auto'  # list, reverse order, timestamp, human readable, hidden file included
 alias ld='ls -dlrth ./*'  # list directories in current directory
@@ -23,6 +23,9 @@ alias less='less -N'  # I like my less command w/line numbers
 alias cat='/usr/bin/bat'  # bat is like a less command but with colors
 alias grep='/usr/bin/grep --color=auto -n'
 
+# prompt line  will look like:
+# 14:25:33 [user@hostname] /current/working/directory
 export PS1="\t \[\e[35m\][\[\e[m\]\[\e[36m\]\u\[\e[m\]@\[\e[34m\]\h\[\e[m\]\[\e[35m\]]\[\e[m\] \[\e[33m\]\w\[\e[m\]\n"  # I like this format, refer to https://ezprompt.net/
 
+# everytime your teminal loads, will run a fastfetch. You can also substitute with the pokemon though, looks cool
 fastfetch
